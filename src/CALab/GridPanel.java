8ec0603ce88bf2479b4dce5826695ccb74dd8772
Tiger <tigeryang2005@gmail.com>
2024-03-05 12:48:08 -0800

    partial GridPanel implementation

--- src/CALab/GridPanel.java
+++ src/CALab/GridPanel.java
@@ -1,4 +1,41 @@
 package CALab;
 
-public class GridPanel {
+import mvc.AppFactory;
+import mvc.AppPanel;
+
+import javax.swing.*;
+
+public class GridPanel extends AppPanel {
+    private JButton run1;
+    private JButton run50;
+    private JButton populate;
+    private JButton clear;
+    public GridPanel(AppFactory factory) {
+        super(factory);
+
+        run1 = new JButton("Run 1");
+        run1.addActionListener(this);
+        getControl().add(run1);
+
+        run50 = new JButton("Run 50");
+        run50.addActionListener(this);
+        getControl().add(run50);
+
+        populate = new JButton("Populate");
+        populate.addActionListener(this);
+        getControl().add(populate);
+
+        clear = new JButton("Clear");
+        clear.addActionListener(this);
+        getControl().add(clear);
+
+        getControl().revalidate();
+    }
+
+    public static void main(String[] args) {
+        AppFactory factory = new StoplightFactory();
+        AppPanel panel = new stopLight.StoplightPanel(factory);
+        //panel.display();
+    }
+
 }
