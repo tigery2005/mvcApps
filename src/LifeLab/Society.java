d08efb31b87b6d32589826edf33a30daf2a06a5a
ALeBD <thealvingold2012@gmail.com>
2024-03-06 12:54:45 -0800

    Life package start

--- src/LifeLab/Society.java
+++ src/LifeLab/Society.java
@@ -8,4 +8,15 @@ public class Society extends Grid {
     public static Set<Integer> rebirth = new HashSet<Integer>();
     public static Set<Integer> death = new HashSet<Integer>();
     public static int percentAlive = 50;
+
+    static {
+        rebirth.add(3);
+        death.add(0);
+        death.add(1);
+        death.add(4);
+        death.add(5);
+        death.add(6);
+        death.add(7);
+        death.add(8);
+    }
 }
