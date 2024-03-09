06d1287713231f8cae30d42903845894f8cd69ab
ALeBD <thealvingold2012@gmail.com>
2024-03-08 21:40:52 -0800

    Cell and Cellview

--- src/CALab/Cell.java
+++ src/CALab/Cell.java
@@ -12,12 +12,33 @@ abstract class Cell extends Publisher implements Serializable {
     protected Set<Cell> neighbors = new HashSet<Cell>();
     protected Grid myGrid = null;
     protected Cell partner = null;
+    protected Color color = Color.red;
 
 
     // choose a random neighbor as a partner
     public void choosePartner() {
         if (partner == null && neighbors != null) {
-			/*
+			partner = null;
+            ArrayList<Cell> arr = new ArrayList <Cell> (neighbors.size());
+            //arr = neighbors.toArray(arr);
+            //Cell[] arr_new = new Cell[arr.length-1];
+
+            Random random = new Random();
+
+            boolean found = false;
+
+            while(!found) {
+                int x = random.nextInt(arr.size());
+                if (arr.get(x).partner == null) {
+                    found = true;
+                    partner = arr.get(x).partner;
+                }
+                //remove arr[0] from temp array
+                else {
+                    arr.remove(x);
+                }
+            }
+            /*
 			Set partner to null
 			Convert neighbors set to a local array
 			Starting at a random position in the array search for a neighbor without a partner
@@ -37,11 +58,9 @@ abstract class Cell extends Publisher implements Serializable {
     }
 
     public Color getColor() {
-        return null; //for now
+        return color; //for now
     }
-    public int getStatus() {
-        return 0; //
-    }
+    public abstract int getStatus();
     // observer neighbors' states
     public abstract void observe();
     // interact with a random neighbor
