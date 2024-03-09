66e1f2b9da741045d927e282108219f05509cc3c
pocafup <61262221+pocafup@users.noreply.github.com>
2024-03-08 20:40:10 -0800

    Add files via upload

---
+++ CALab/GridView.java
@@ -0,0 +1,28 @@
+package CALab;
+
+import javax.swing.*;
+
+import mvc.*;
+import java.awt.*;
+
+public class GridView  extends View {
+
+    private CellView cellViews[][];
+
+    private int row;
+
+    private int col;
+
+    public GridView(Model model) {
+        super(model);
+        CellView cell = new CellView(((Grid)model).getCell(row, col);
+        cellViews[row][col] = cell;
+
+    }
+
+    public void update(String msg, Object oldState, Object newState) {
+        // call update method of each CellView
+        for (int i=0;i<row;i++) for (CellView u : cellViews[i]) u.update(msg,oldState,newState);
+    }
+
+}
